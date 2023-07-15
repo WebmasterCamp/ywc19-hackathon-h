41bee74494e4c50ee1ecf03bfe84768f8952b6d4
ninew089 <ninew.jck@gmail.com>
2023-07-15 17:17:54 +0700

    feat: animation and useMediaQuery hook

---
+++ src/components/animate/MotionViewport.tsx
@@ -0,0 +1,42 @@
+import { ReactNode } from "react";
+import { m, MotionProps } from "framer-motion";
+
+//
+import { varContainer } from ".";
+import { useMediaQuery } from "@ywc19/hooks/useMediaQuery";
+
+// ----------------------------------------------------------------------
+
+type IProps = MotionProps;
+
+interface Props extends IProps {
+  children: ReactNode;
+  disableAnimatedMobile?: boolean;
+  className?: string;
+}
+
+export default function MotionViewport({
+  children,
+  disableAnimatedMobile = true,
+  className,
+  ...other
+}: Props) {
+  const isMobile = useMediaQuery("(max-width: 767px)");
+
+  if (isMobile && disableAnimatedMobile) {
+    return <div className={className}>{children}</div>;
+  }
+
+  return (
+    <m.div
+      initial="initial"
+      whileInView="animate"
+      className={className}
+      viewport={{ once: true, amount: 0.3 }}
+      variants={varContainer()}
+      {...other}
+    >
+      {children}
+    </m.div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "@ywc19/utils/api";
+import { LazyImage } from "@ywc19/components/LazyImage";
+import { MotionContainer, varFade } from "@ywc19/components/animate";
+import { m } from "framer-motion";
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -24,47 +27,6 @@ export default function Home() {
               href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
             >
-              <form>
-                <label
-                  for="default-search"
-                  className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Search
-                </label>
-                <div className="relative">
-                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                    <svg
-                      className="h-4 w-4 text-gray-500 dark:text-gray-400"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                      />
-                    </svg>
-                  </div>
-                  <input
-                    type="search"
-                    id="default-search"
-                    className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                    placeholder="Search Mockups, Logos..."
-                    required
-                  />
-                  <button
-                    type="submit"
-                    className="absolute bottom-2.5 right-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Search
-                  </button>
-                </div>
-              </form>
-
               <h3 className="text-2xl font-bold">First Steps →</h3>
               <div className="text-lg">
                 Just the basics - Everything you need to know to set up your
@@ -83,6 +45,22 @@ export default function Home() {
               </div>
             </Link>
           </div>
+          <MotionContainer>
+            <m.h1
+              variants={varFade().inLeft}
+              className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]"
+            >
+              Test
+            </m.h1>
+            <LazyImage
+              ratio="16/9"
+              src={
+                "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+              }
+              className="w-full"
+              alt={"ywx"}
+            />
+          </MotionContainer>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
               {hello.data ? hello.data.greeting : "Loading tRPC query..."}
