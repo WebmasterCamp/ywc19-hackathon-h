a52bf6913e38fe6645c1cf031b25c7e26f3cf687
ninew089 <ninew.jck@gmail.com>
2023-07-16 10:38:15 +0700

    feat: add click away

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -1,10 +1,17 @@
 import { CardRestaurant } from "@ywc19/components/CardRestaurant";
 import { CardMenu } from "@ywc19/components/CardMenu";
+import { useState } from "react";
+import ClickAwayListener from "react-click-away-listener";
 
 export default function Home() {
   const mock = [1, 2, 3, 4];
   const mockMenu = [1, 2, 3, 4, 5, 6, 7, 8];
-
+  const [openTag, setOpentag] = useState(false);
+  const [selectTag, setSelectTag] = useState<string>();
+  const [openCategory, setOpenCategory] = useState(false);
+  const [selectCategory, setSelectCategory] = useState<string>();
+  const handleClickAway = () => setOpentag(false);
+  const handleCategoryAway = () => setOpenCategory(false);
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center  pb-[84px]">
@@ -14,7 +21,7 @@ export default function Home() {
               <label className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 Search
               </label>
-              <div className="relative ">
+              <div className="relative">
                 <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                   <svg
                     width="24"
@@ -61,69 +68,106 @@ export default function Home() {
               </svg>
               Nearby
             </button>
-            <button
-              data-modal-hide="popup-modal"
-              type="button"
-              className="flex h-10 w-fit items-center justify-center gap-x-2 rounded-full border border-[#212B36] px-5 py-2.5 text-center text-sm font-medium text-[#212B36]"
-            >
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M2.5 5.83333C2.5 6.29167 2.875 6.66667 3.33333 6.66667H16.6667C17.125 6.66667 17.5 6.29167 17.5 5.83333C17.5 5.375 17.125 5 16.6667 5H3.33333C2.875 5 2.5 5.375 2.5 5.83333ZM9.16667 15H10.8333C11.2917 15 11.6667 14.625 11.6667 14.1667C11.6667 13.7083 11.2917 13.3333 10.8333 13.3333H9.16667C8.70833 13.3333 8.33333 13.7083 8.33333 14.1667C8.33333 14.625 8.70833 15 9.16667 15ZM14.1667 10.8333H5.83333C5.375 10.8333 5 10.4583 5 10C5 9.54167 5.375 9.16667 5.83333 9.16667H14.1667C14.625 9.16667 15 9.54167 15 10C15 10.4583 14.625 10.8333 14.1667 10.8333Z"
-                  fill="#212B36"
-                />
-              </svg>
-              Price
-            </button>
-            <button
-              data-modal-hide="popup-modal"
-              type="button"
-              className="flex h-10 w-fit items-center justify-center gap-x-2 rounded-full border border-[#212B36] px-5 py-2.5 text-center text-sm font-medium text-[#212B36]"
-            >
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M2.5 5.83333C2.5 6.29167 2.875 6.66667 3.33333 6.66667H16.6667C17.125 6.66667 17.5 6.29167 17.5 5.83333C17.5 5.375 17.125 5 16.6667 5H3.33333C2.875 5 2.5 5.375 2.5 5.83333ZM9.16667 15H10.8333C11.2917 15 11.6667 14.625 11.6667 14.1667C11.6667 13.7083 11.2917 13.3333 10.8333 13.3333H9.16667C8.70833 13.3333 8.33333 13.7083 8.33333 14.1667C8.33333 14.625 8.70833 15 9.16667 15ZM14.1667 10.8333H5.83333C5.375 10.8333 5 10.4583 5 10C5 9.54167 5.375 9.16667 5.83333 9.16667H14.1667C14.625 9.16667 15 9.54167 15 10C15 10.4583 14.625 10.8333 14.1667 10.8333Z"
-                  fill="#212B36"
-                />
-              </svg>
-              Tag
-            </button>
-            <button
-              data-modal-hide="popup-modal"
-              type="button"
-              className="flex h-10 w-fit items-center justify-center gap-x-2 rounded-full border border-[#212B36] px-5 py-2.5 text-center text-sm font-medium text-[#212B36]"
-            >
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M2.5 5.83333C2.5 6.29167 2.875 6.66667 3.33333 6.66667H16.6667C17.125 6.66667 17.5 6.29167 17.5 5.83333C17.5 5.375 17.125 5 16.6667 5H3.33333C2.875 5 2.5 5.375 2.5 5.83333ZM9.16667 15H10.8333C11.2917 15 11.6667 14.625 11.6667 14.1667C11.6667 13.7083 11.2917 13.3333 10.8333 13.3333H9.16667C8.70833 13.3333 8.33333 13.7083 8.33333 14.1667C8.33333 14.625 8.70833 15 9.16667 15ZM14.1667 10.8333H5.83333C5.375 10.8333 5 10.4583 5 10C5 9.54167 5.375 9.16667 5.83333 9.16667H14.1667C14.625 9.16667 15 9.54167 15 10C15 10.4583 14.625 10.8333 14.1667 10.8333Z"
-                  fill="#212B36"
-                />
-              </svg>
-              Category
-            </button>
+            <ClickAwayListener onClickAway={handleClickAway}>
+              <div className="relative">
+                <button
+                  data-modal-hide="popup-modal"
+                  type="button"
+                  onClick={() => setOpentag(!openTag)}
+                  className="flex h-10 w-fit items-center justify-center gap-x-2 rounded-full border border-[#212B36] px-5 py-2.5 text-center text-sm font-medium text-[#212B36]"
+                >
+                  <svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M2.5 5.83333C2.5 6.29167 2.875 6.66667 3.33333 6.66667H16.6667C17.125 6.66667 17.5 6.29167 17.5 5.83333C17.5 5.375 17.125 5 16.6667 5H3.33333C2.875 5 2.5 5.375 2.5 5.83333ZM9.16667 15H10.8333C11.2917 15 11.6667 14.625 11.6667 14.1667C11.6667 13.7083 11.2917 13.3333 10.8333 13.3333H9.16667C8.70833 13.3333 8.33333 13.7083 8.33333 14.1667C8.33333 14.625 8.70833 15 9.16667 15ZM14.1667 10.8333H5.83333C5.375 10.8333 5 10.4583 5 10C5 9.54167 5.375 9.16667 5.83333 9.16667H14.1667C14.625 9.16667 15 9.54167 15 10C15 10.4583 14.625 10.8333 14.1667 10.8333Z"
+                      fill="#212B36"
+                    />
+                  </svg>
+                  Tag
+                </button>
+                {openTag && (
+                  <div
+                    id="dropdown"
+                    className="absolute  z-10 w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
+                  >
+                    <ul
+                      className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                      aria-labelledby="dropdownDefaultButton"
+                    >
+                      <li>
+                        <button
+                          onClick={() => setSelectTag(" Gluten-Free")}
+                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          Gluten-Free
+                        </button>
+                      </li>
+                      <li>
+                        <button
+                          onClick={() => setSelectTag("Vegan")}
+                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          Vegan
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </ClickAwayListener>
+            <ClickAwayListener onClickAway={handleCategoryAway}>
+              <div className="relative">
+                <button
+                  data-modal-hide="popup-modal"
+                  type="button"
+                  onClick={() => setOpenCategory(!openTag)}
+                  className="flex h-10 w-fit items-center justify-center gap-x-2 rounded-full border border-[#212B36] px-5 py-2.5 text-center text-sm font-medium text-[#212B36]"
+                >
+                  <svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M2.5 5.83333C2.5 6.29167 2.875 6.66667 3.33333 6.66667H16.6667C17.125 6.66667 17.5 6.29167 17.5 5.83333C17.5 5.375 17.125 5 16.6667 5H3.33333C2.875 5 2.5 5.375 2.5 5.83333ZM9.16667 15H10.8333C11.2917 15 11.6667 14.625 11.6667 14.1667C11.6667 13.7083 11.2917 13.3333 10.8333 13.3333H9.16667C8.70833 13.3333 8.33333 13.7083 8.33333 14.1667C8.33333 14.625 8.70833 15 9.16667 15ZM14.1667 10.8333H5.83333C5.375 10.8333 5 10.4583 5 10C5 9.54167 5.375 9.16667 5.83333 9.16667H14.1667C14.625 9.16667 15 9.54167 15 10C15 10.4583 14.625 10.8333 14.1667 10.8333Z"
+                      fill="#212B36"
+                    />
+                  </svg>
+                  Category
+                </button>
+                {openCategory && (
+                  <div
+                    id="dropdown"
+                    className="absolute  z-10 w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
+                  >
+                    <ul
+                      className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                      aria-labelledby="dropdownDefaultButton"
+                    >
+                      <li>
+                        <button
+                          onClick={() => setSelectCategory("Vegan")}
+                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          Vegan
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </ClickAwayListener>
           </div>
           <p className="mb-10 text-h3 font-bold">8 Results for “Vegan”</p>
           <p className="mb-10 ml-2 text-h3 font-bold">
